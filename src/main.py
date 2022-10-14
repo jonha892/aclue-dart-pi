0fdbdbcc4a8f3f35fffe4f4e7f754668745db42b
jonha892 <jhalstrup@googlemail.com>
2022-10-14 16:11:36 +0200

    add new resolution api

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import platform
 from pprint import pprint
 import base64
 import os
+from turtle import width
 
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
@@ -13,12 +14,18 @@ DATA_PATH = "../data"
 app = FastAPI()
 
 camera = None
+width = 1024
+height = 720
+
+headers = {"Access-Control-Allow-Origin": "*"}
+
 if platform.machine() == "armv7l":
     print("RaspberryPi detected, setting up PI specific paths")
 
     from picamera import PiCamera
+
     camera = PiCamera()
-    camera.resolution = (1024,720)
+    camera.resolution = (width, height)
     camera.start_preview()
     sleep(2)
 
@@ -26,9 +33,19 @@ if platform.machine() == "armv7l":
     async def take_image():
         img_byte_stream = BytesIO()
         camera.capture(img_byte_stream, "png")
-        
-        headers = { 'Access-Control-Allow-Origin': '*' }
-        return Response(content=img_byte_stream.getvalue(), media_type="image/png", headers=headers)
+
+        return Response(
+            content=img_byte_stream.getvalue(), media_type="image/png", headers=headers
+        )
+
+    @app.get("/resolution")
+    async def resolution():
+        content = {
+            "width": width,
+            "height": height,
+        }
+
+        return Response(content=content, headers=headers)
 
     @app.put("led-green")
     async def led_green():
@@ -37,8 +54,10 @@ if platform.machine() == "armv7l":
     @app.put("led-red")
     async def led_green():
         pass
+
 else:
     print("WARN: PI not detected. API is limited!")
+
 
 @app.get("/")
 async def root():
