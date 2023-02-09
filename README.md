ef940bc96d891cffc39f92aed83ae353678df5c2
jonha892 <jhalstrup@googlemail.com>
2023-02-09 17:40:39 +0100

    init. OpenCV support

--- README.md
+++ README.md
@@ -1,12 +1,69 @@
 # aclue-dart-pi
 
+Dieses Projekt stelle eine einfache API bereit um über eine Webcam oder ein Camera Modul ein Bild aufzunehmen.
+Gedacht ist ein Einsatz auf einem RaspberryPi. 
+
+## Dependencies
+
+### With poetry
 ```
 poetry install
 poetry install --extras runtime # falls auf raspberry pi
 poetry shell
+````
 
+### With `venv`
+
+```
+python3 -m venv env
+source env/bin/activate
+pip3 install xxx
+
+```
+
+
+### OpenCV
+Die aktuelle Version kann aktuell (09.02.2023) nicht installiert werden. Als workaround nutzen wir eine ältere Version.
+
+```
+# this one doesn't work:
+pip3 install --trusted-host pypi.org --trusted-host files.pythonhosted.org --trusted-host www.piwheels.org opencv-python
+
+# this one does
+pip3 install opencv-python==4.5.3.56 --trusted-host www.piwheels.org
+```
+
+OpenCV braucht manche dependencies welche nicht per default auf dem RaspberryPI installiert sind.
+
+```
+# open_cv dependencies
+
+sudo apt-get install libcblas-dev
+sudo apt-get install libhdf5-dev
+sudo apt-get install libhdf5-serial-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libjasper-dev 
+sudo apt-get install libqtgui4 
+sudo apt-get install libqt4-test
+```
+
+## Sync code Notebook <=> RaspberryPi
+```
+rsync -av --delete src/ pi@raspberrypi:~/dev/aclue-dart-pi/src/
+```
+
+## Server starten
+
+```
 cd src
 uvicorn main:app --reload --host=0.0.0.0
 
 => http://localhost:8000/
+```
+
+
+## API Latenz messen
+```
+Get Image time:
+curl -o /dev/null -s -w 'Total: %{time_total}s\n' http://raspberrypi:8000/api/imagev2
 ```
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+import io
 from time import sleep
 import platform
 from pprint import pprint
@@ -6,17 +7,31 @@ import base64
 import os
 import json
 
+import cv2 as cv
+import numpy as np
+from PIL import Image
 from fastapi import FastAPI, Response
 
 app = FastAPI()
 
 camera = None
-width = 1024
-height = 720
+# width = 1024
+# height = 720
+width = 1920
+height = 1024
+
+cap = cv.VideoCapture(0)
+cap.set(3, width)
+cap.set(4, height)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
 
 headers = {"Access-Control-Allow-Origin": "*"}
 
-if platform.machine() == "armv7l":
+with_pi_camera = False
+if platform.machine() == "armv7l" and with_pi_camera:
     print("RaspberryPi detected, setting up PI specific paths")
 
     from picamera import PiCamera
@@ -51,11 +66,32 @@ if platform.machine() == "armv7l":
     @app.put("led-red")
     async def led_green():
         pass
-
 else:
     print("WARN: PI not detected. API is limited!")
 
+@app.get("/api/imagev2")
+async def take_image_v2():
+    _ret, frame = cap.read()
+    #print(_ret, frame)
+    print(_ret, "frame shape: ", frame.shape)
+    # height, width, channels = frame.shape
+    
+    #img_encoded = cv.imencode(".png", frame)[1]
+    shifted = np.zeros(frame.shape, dtype=frame.dtype)
+    shifted[:,:,0] = frame[:,:, 2]
+    shifted[:,:,1] = frame[:,:, 1]
+    shifted[:,:,2] = frame[:,:, 0]
 
+    im = Image.fromarray(shifted)
+    
+    # save image to an in-memory bytes buffer
+    with io.BytesIO() as buf:
+        im.save(buf, format='PNG')
+        im_bytes = buf.getvalue()
+
+    return Response(
+            content=im_bytes, media_type="image/png", headers=headers
+        )
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
